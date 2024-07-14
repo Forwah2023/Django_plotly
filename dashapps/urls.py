6269e26ab3377ceb756e15b0913702f156eb6f2c
Forwah Amstrong <lmsoftware2023@gmail.com>
2024-07-14 10:22:32 +0100

    added about page and updated css
    -Moved css from in-file to static folder
    -added about.html and configured corresponding urls
    -added my profile pic

--- dashapps/urls.py
+++ dashapps/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path,include 
-from .views import HomeView
+from .views import HomeView,AboutView
 
 
 urlpatterns = [
 path('', HomeView.as_view(), name='home'),
+path('about', AboutView.as_view(), name='about'),
 path('django_plotly_dash/', include('django_plotly_dash.urls')),
 ]
