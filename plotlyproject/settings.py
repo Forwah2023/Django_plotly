3e109017754a8512b299ce199e3f68bad9f33dbc
Forwah Amstrong <lmsoftware2023@gmail.com>
2025-08-11 15:55:06 +0100

    added support for Render deploy db

--- plotlyproject/settings.py
+++ plotlyproject/settings.py
@@ -22,119 +22,134 @@ env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env("DEBUG")
+# SQLITE in Production setting
+SQLITEINPRODUCTION = env("SQLITEINPRODUCTION")
 
-ALLOWED_HOSTS = ['secure-peak-71101-588cd4b50a6d.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    "secure-peak-71101-588cd4b50a6d.herokuapp.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
-    #third party
-    'dashapps.apps.DashappsConfig',
-    'django_plotly_dash.apps.DjangoPlotlyDashConfig',
-    'dpd_static_support',
-    'django_bootstrap5',
-	'debug_toolbar',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "whitenoise.runserver_nostatic",
+    "django.contrib.staticfiles",
+    # third party
+    "dashapps.apps.DashappsConfig",
+    "django_plotly_dash.apps.DjangoPlotlyDashConfig",
+    "dpd_static_support",
+    "django_bootstrap5",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_plotly_dash.middleware.BaseMiddleware',
-    'django_plotly_dash.middleware.ExternalRedirectionMiddleware',
-	'debug_toolbar.middleware.DebugToolbarMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_plotly_dash.middleware.BaseMiddleware",
+    "django_plotly_dash.middleware.ExternalRedirectionMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
-ROOT_URLCONF = 'plotlyproject.urls'
+ROOT_URLCONF = "plotlyproject.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'plotlyproject.wsgi.application'
+WSGI_APPLICATION = "plotlyproject.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-ON_HEROKU = 'DYNO' in os.environ
+ON_HEROKU = "DYNO" in os.environ
 
-IN_DOCKER=env('IN_DOCKER',default=False)
+IN_DOCKER = env("IN_DOCKER", default=False)
+ON_RENDER=env("ON_RENDER", default=False)
 
-if ON_HEROKU:
+if ON_HEROKU or ON_RENDER:
     # Heroku PostgreSQL configuration
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('DATABASE_URL'),
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("DATABASE_URL"),
         }
     }
     # Heroku database config from dj_database_url
     import dj_database_url
+
     db_from_env = dj_database_url.config(
-                    conn_max_age=500,
-                    conn_health_checks=True,
-                    )
-    DATABASES['default'].update(db_from_env)
-elif IN_DOCKER :
-	# Local development in Docker configuration
-	DATABASES = {
-		'default': {
-			'ENGINE': 'django.db.backends.postgresql',
-			'NAME': 'postgres',
-			'USER': 'postgres',
-			'PASSWORD' : 'postgres',
-			'HOST':'db',
-			'PORT': 5432
-		}
-	}
+        conn_max_age=500,
+        conn_health_checks=True,
+    )
+    DATABASES["default"].update(db_from_env)
+elif IN_DOCKER:
+    # Local development in Docker configuration
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "postgres",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "db",
+            "PORT": 5432,
+        }
+    }
+elif SQLITEINPRODUCTION:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 else:
-	# Local development configuration
-	DATABASES = {
-		'default': {
-			'ENGINE': 'django.db.backends.postgresql',
-			'NAME': 'Dvdb',
-			'USER': env('DBUSER'),
-			'PASSWORD':env('DBPASSWORD'), 
-			'HOST': 'localhost',
-			'PORT': 5432
-		}
-	}
+    # Local development configuration
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "Dvdb",
+            "USER": env("DBUSER"),
+            "PASSWORD": env("DBPASSWORD"),
+            "HOST": "localhost",
+            "PORT": 5432,
+        }
+    }
 
 
 # Password validation
@@ -142,16 +157,16 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -159,9 +174,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -171,8 +186,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STORAGES = {
     "staticfiles": {
@@ -180,89 +197,77 @@ STORAGES = {
     },
 }
 STATICFILES_FINDERS = [
-
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-
-    'django_plotly_dash.finders.DashAssetFinder',
-    'django_plotly_dash.finders.DashComponentFinder',
-    'django_plotly_dash.finders.DashAppDirectoryFinder',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    "django_plotly_dash.finders.DashAssetFinder",
+    "django_plotly_dash.finders.DashComponentFinder",
+    "django_plotly_dash.finders.DashAppDirectoryFinder",
 ]
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # DJANGO-PLOTLY
-X_FRAME_OPTIONS = 'SAMEORIGIN'
+X_FRAME_OPTIONS = "SAMEORIGIN"
 
 PLOTLY_DASH = {
-
     # Route used for the message pipe websocket connection
-    "ws_route" :   "dpd/ws/channel",
-
+    "ws_route": "dpd/ws/channel",
     # Route used for direct http insertion of pipe messages
-    "http_route" : "dpd/views",
-
+    "http_route": "dpd/views",
     # Flag controlling existince of http poke endpoint
-    "http_poke_enabled" : True,
-
+    "http_poke_enabled": True,
     # Insert data for the demo when migrating
-    "insert_demo_migrations" : False,
-
+    "insert_demo_migrations": False,
     # Timeout for caching of initial arguments in seconds
     "cache_timeout_initial_arguments": 60,
-
     # Name of view wrapping function
     "view_decorator": None,
-
     # Flag to control location of initial argument storage
     "cache_arguments": True,
-
     # Flag controlling local serving of assets
     "serve_locally": True,
 }
 
 
 PLOTLY_COMPONENTS = [
-
     # Common components (ie within dash itself) are automatically added
-
     # django-plotly-dash components
-    'dpd_components',
+    "dpd_components",
     # static support if serving local assets
-    'dpd_static_support',
-
+    "dpd_static_support",
     # Other components, as needed
-    'dash_bootstrap_components',
+    "dash_bootstrap_components",
 ]
 
 
-ENVIRONMENT=env('ENVIRONMENT',default='development')
+ENVIRONMENT = env("ENVIRONMENT", default="development")
 
-if ENVIRONMENT == 'production':
-	#XSS broswer protection
-	SECURE_BROWSER_XSS_FILTER = True
-	X_FRAME_OPTIONS = 'DENY'
-	SECURE_SSL_REDIRECT = True
-	SECURE_HSTS_SECONDS = 3600 
-	SECURE_HSTS_INCLUDE_SUBDOMAINS = True 
-	SECURE_HSTS_PRELOAD = True 
-	SECURE_CONTENT_TYPE_NOSNIFF = True
-	SESSION_COOKIE_SECURE = True
-	CSRF_COOKIE_SECURE = True
-	SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    
+if ENVIRONMENT == "production":
+    # XSS broswer protection
+    SECURE_BROWSER_XSS_FILTER = True
+    X_FRAME_OPTIONS = "DENY"
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 3600
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
 
 if not IN_DOCKER:
     # django-debug-toolbar for Non-Docker
-     INTERNAL_IPS = ['127.0.0.1']  
-else:     
+    INTERNAL_IPS = ["127.0.0.1"]
+else:
     # django-debug-toolbar for Docker
     import socket
+
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
